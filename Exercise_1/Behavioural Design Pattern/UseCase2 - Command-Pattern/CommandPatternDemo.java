cf943da3b53782704e04842aaad1087593b61b63
VedaSampreetha-04 <99634087+VedaSampreetha-04@users.noreply.github.com>
2024-10-02 17:23:59 +0530

    Add files via upload

---
+++ Exercise_1/Behavioural Design Pattern/UseCase2 - Command-Pattern/CommandPatternDemo.java
@@ -0,0 +1,75 @@
+// Command interface
+interface Command {
+    void execute();
+}
+
+// Concrete Commands
+class LightOnCommand implements Command {
+    private Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOn();
+    }
+}
+
+class LightOffCommand implements Command {
+    private Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOff();
+    }
+}
+
+// Receiver
+class Light {
+    public void turnOn() {
+        System.out.println("Light is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("Light is OFF");
+    }
+}
+
+// Invoker
+class RemoteControl {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+    }
+}
+
+// Demonstration
+public class CommandPatternDemo {
+    public static void main(String[] args) {
+        Light light = new Light();
+
+        Command lightOn = new LightOnCommand(light);
+        Command lightOff = new LightOffCommand(light);
+
+        RemoteControl remote = new RemoteControl();
+
+        // Turn the light on
+        remote.setCommand(lightOn);
+        remote.pressButton();
+
+        // Turn the light off
+        remote.setCommand(lightOff);
+        remote.pressButton();
+    }
+}
